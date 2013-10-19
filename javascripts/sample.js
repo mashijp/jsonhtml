d2377dd3e8b3a3be74114af4c42bbd6952154a98
mashijp <mashijp@gmail.com>
2013-10-19 12:44:04 +0900

    コピペ用インデント生成

--- javascripts/sample.js
+++ javascripts/sample.js
@@ -26,28 +26,27 @@ jQuery(function($){
         var result = $("<dl/>");
         if (key !== null) {
             var dt = $("<dt/>");
+            dt.append(createIndent(nestLevel));
             dt.append('"');
             dt.append($("<div/>").addClass("content").text(key));
             dt.append('": ');
 
             result.append(dt);
         }
-        var dd = $("<dd/>");
         var type = value === null ? "null" : typeof value;
         if (type === "object" && value.length !== undefined) type = "array" // typeof [] ... arrayなのでがんばって判定
+
+        var dd = $("<dd/>");
         var ddContent = $("<div/>").addClass("content");
         dd.append(ddContent);
         switch (type) {
-            case "null":
-                result.addClass("js-null");
-                ddContent.text(value);
-                break;
             case "object":
                 result.addClass("js-object");
                 dd.prepend('{');
                 $.each(value, function(key, value){
                     ddContent.append(createDl(key, value, nestLevel + 1));
                 });
+                dd.append(createIndent(nestLevel));
                 dd.append('}');
                 break;
             case "array":
@@ -56,7 +55,12 @@ jQuery(function($){
                 $.each(value, function(key, value){
                     ddContent.append(createDl(null, value, nestLevel + 1));
                 });
+                dd.append(createIndent(nestLevel));
                 dd.append(']');
+                break;
+            case "null":
+                result.addClass("js-null");
+                ddContent.text(value);
                 break;
             case "string":
                 result.addClass("js-string");
@@ -72,7 +76,15 @@ jQuery(function($){
                 alert(type);
         }
         dd.append($("<span/>").text(",").addClass("separator"));
+
+        if (key === null) dd.prepend(createIndent(nestLevel));
+
         result.append(dd);
+        return result;
+    }
+
+    function createIndent(nestLevel) {
+        var result = $("<span/>").addClass("hidden").html(new Array(nestLevel * 2 + 1).join("&nbsp;"));
         return result;
     }
 
